06f6bfceca59687bb21ff12cb4579011233a62e4
Almost18 <55597290+Almos18@users.noreply.github.com>
2020-12-05 02:00:04 +0000

    Project Commit 9

--- Encryption Project/EncryptionV2/EncryptObject.java
+++ Encryption Project/EncryptionV2/EncryptObject.java
@@ -1,74 +1,39 @@
 package EncryptionV2;
 
-import javax.swing.*;
-import java.util.Arrays;
+public class EncryptObject{
 
-public class EncryptObject {
+    private String encryptedMessages;
+    private String decryptedMessages;
 
-    private String encryptedMessages[] = new String[10];
-    private String decryptedMessages[] = new String[10];
-    //private String username;
-   // private String password;
-
-    public EncryptObject(String[] encryptedMessages, String[] decryptedMessages)
-    {
-        this(new String[]{"No encrypted messages to show"},new String[]{"No decrypted messages to show"},"none","none");
+    public EncryptObject() {
+        this("None", "None");
     }
 
-    public EncryptObject(String[] encryptedMessages, String[] decryptedMessages, String username, String password)
-    {
+    public EncryptObject(String encryptedMessages, String decryptedMessages) {
         setEncryptedMessages(encryptedMessages);
         setDecryptedMessages(decryptedMessages);
         toString();
-       // setUsername(username);
-        //setPassword(password);
     }
 
-    public String[] getEncryptedMessages() {
-
+    public String getEncryptedMessages() {
         return encryptedMessages;
-
     }
 
-    public void setEncryptedMessages(String[] encryptedMessages)
-    {
+    public void setEncryptedMessages(String encryptedMessages) {
         this.encryptedMessages = encryptedMessages;
     }
 
-    public String[] getDecryptedMessages() {
+    public String getDecryptedMessages() {
         return decryptedMessages;
     }
 
-    public void setDecryptedMessages(String[] decryptedMessages) {
+    public void setDecryptedMessages(String decryptedMessages) {
         this.decryptedMessages = decryptedMessages;
     }
 
     @Override
     public String toString() {
-        return "Encrypted Messages: " + Arrays.toString(getEncryptedMessages()) +
-                "Decrypted Messages: " + Arrays.toString(getDecryptedMessages());
+        return "Encrypted Messages: " + getEncryptedMessages() +
+                "\nDecrypted Messages: " + getDecryptedMessages();
     }
-    /* public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;*/
-    }
-
-/*encryptTemp = JOptionPane.showInputDialog(null,"Please input a message you want encrypted",
-            "Question",JOptionPane.QUESTION_MESSAGE);*/
-
-    /*decryptTemp = JOptionPane.showInputDialog(null,"Please input a message you want encrypted",
-            "Question",JOptionPane.QUESTION_MESSAGE);*/
-
-    //This is experimenting
-//}
+}
--- Encryption Project/EncryptionV2/EncryptionUI.java
+++ Encryption Project/EncryptionV2/EncryptionUI.java
@@ -2,29 +2,33 @@ package EncryptionV2;
 
 import java.awt.*;
 import java.awt.event.*;
-import java.util.Arrays;
+import java.util.*;
 import javax.swing.*;
 
-public class EncryptionUI extends JFrame implements ActionListener{
+public class EncryptionUI extends JFrame implements ActionListener {
 
     JButton encryptButton;
     JButton decryptButton;
+    JFrame jFrameWindow;
     JMenu fileMenu;
     JMenu editMenu;
     JLabel passwordLabel;
     JLabel usernameLabel;
-    JTextField passwordField;
+    JPasswordField passwordField;
     JTextField usernameField;
-    String[] encryptedMessages = new String[10];
-    String[] decryptedMessages = new String[10];
-    int i = 0;
-    int j = 0;
+    String encryptMessageVariable;
+    String decryptMessageVariable;
+    char[] password;
+    JTextArea recordedMessages;
+    ArrayList<String> mainArray = new ArrayList<String>();
 
-    public EncryptionUI() {
+    //https://www.w3schools.com/java/java_arraylist.asp
 
+    //OOP labsheets
+
+    public EncryptionUI()
+    {
         super("Encryptor");
-
-        EncryptObject main = new EncryptObject(encryptedMessages, decryptedMessages);
 
         setLayout(new GridBagLayout());
 
@@ -39,12 +43,10 @@ public class EncryptionUI extends JFrame implements ActionListener{
         menuBar.add(fileMenu);
         menuBar.add(editMenu);
 
-        //setLocation(200,150);
+        setLocation(700,350);
         setSize(450, 300);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setVisible(true);
-
-
+        setVisible(false);
     }
 
     public static void main(String args[])
@@ -52,45 +54,127 @@ public class EncryptionUI extends JFrame implements ActionListener{
         EncryptionUI guiApp = new EncryptionUI();
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e)
+    private class TextFieldEventHandler implements ActionListener
     {
-
-    }
-        private class ButtonEventHandler implements ActionListener
+        public void actionPerformed(ActionEvent e)
         {
-            public void actionPerformed(ActionEvent e)
+            if(e.getSource() == usernameField)
             {
-                if(e.getSource()==encryptButton)
+                passwordField.requestFocus();
+            }
+            if(e.getSource() == passwordField)
+            {
+                if(passwordField.getPassword().equals(""))
                 {
-                    encryptedMessages[i] = EncryptMessage.encryptString(encryptButton.getText());
-                    i++;
-                    JOptionPane.showMessageDialog(null,Arrays.toString(encryptedMessages));
+                    JOptionPane.showMessageDialog(null, "You have to at least write something",
+                    "Error", JOptionPane.ERROR_MESSAGE);
                 }
-                else if(e.getSource()==decryptButton)
-                {
-                    decryptedMessages[j] = DecryptMessage.decryptString(decryptButton.getText());
-                    j++;
-                    JOptionPane.showMessageDialog(null,Arrays.toString(decryptedMessages));
-                }
+                else
+                    password = passwordField.getPassword();
+                    setVisible(true);
+                    jFrameWindow.dispose();
+
+                    JOptionPane.showMessageDialog(null, "Welcome to the system",
+                    "Success", JOptionPane.INFORMATION_MESSAGE);
+
+
             }
         }
+    }
 
-        private void createLoginMenu()
+    private class ButtonEventHandler implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
         {
-            JFrame jFrameWindow = new JFrame("Login");
+            if (e.getSource() == encryptButton)
+            {
+                encryptMessageVariable = JOptionPane.showInputDialog("Enter a message you wish to encrypt");
+                EncryptObject done = new EncryptObject(EncryptMessage.encryptString(encryptMessageVariable),
+                encryptMessageVariable);
 
-            FlowLayout flowLayout = new FlowLayout();
+                mainArray.add(done.getEncryptedMessages());
+                mainArray.add(done.getDecryptedMessages());
 
-            jFrameWindow.setLayout(flowLayout);
-            jFrameWindow.setSize(300,200);
-            jFrameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            }
+            else if (e.getSource() == decryptButton)
+            {
+                decryptMessageVariable = JOptionPane.showInputDialog("Enter a message you wish to decrypt");
+                EncryptObject done = new EncryptObject(DecryptMessage.decryptString(decryptMessageVariable),
+                decryptMessageVariable);
 
+                mainArray.add(done.getEncryptedMessages());
+                mainArray.add(done.getDecryptedMessages());
+            }
+        }
+    }
 
+    public void actionPerformed(ActionEvent e) {
+        String menuSelect;
+        menuSelect = e.getActionCommand();
+
+        if (menuSelect.equals("Quit")) {
+            System.exit(0);
         }
 
-        private void createEncryptionMenu()
+        else if(menuSelect.equals("Stored Messages"))
         {
+            recordedMessages = new JTextArea(10, 10);
+            recordedMessages.setText("The values you have inputted are:\n\n" + mainArray);
+            recordedMessages.setWrapStyleWord(true);
+            recordedMessages.setLineWrap(true);
+            recordedMessages.setCaretPosition(0);
+            recordedMessages.setEditable(false);
+
+            JOptionPane.showMessageDialog(null,new JScrollPane(recordedMessages),
+            "RESULT",JOptionPane.INFORMATION_MESSAGE);
+        }
+
+        else if(menuSelect.equals("Username"))
+        {
+            JOptionPane.showMessageDialog(null,"Your username is " + usernameField.getText(),
+            "Username",JOptionPane.INFORMATION_MESSAGE);
+        }
+
+        else if(menuSelect.equals("Password"))
+        {
+            JOptionPane.showMessageDialog(null,"Your password is " + password,
+            "Password",JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+
+    private void createLoginMenu()
+    {
+        jFrameWindow = new JFrame("Login");
+
+        FlowLayout flowLayout = new FlowLayout();
+
+        jFrameWindow.setLayout(flowLayout);
+        jFrameWindow.setSize(300, 150);
+        jFrameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrameWindow.setLocation(700,350);
+
+        usernameLabel = new JLabel("Username:");
+        jFrameWindow.add(usernameLabel);
+
+        usernameField = new JTextField(15);
+        jFrameWindow.add(usernameField);
+
+        passwordLabel = new JLabel("Password:");
+        jFrameWindow.add(passwordLabel);
+
+        passwordField = new JPasswordField(15);
+        jFrameWindow.add(passwordField);
+
+        EncryptionUI.TextFieldEventHandler handler2 = new EncryptionUI.TextFieldEventHandler();
+
+        usernameField.addActionListener(handler2);
+        passwordField.addActionListener(handler2);
+
+        jFrameWindow.setVisible(true);
+    }
+
+    private void createEncryptionMenu()
+    {
         JMenuItem item;
 
         fileMenu = new JMenu("View");
@@ -120,9 +204,9 @@ public class EncryptionUI extends JFrame implements ActionListener{
         item = new JMenuItem("Quit");
         item.addActionListener(this);
         fileMenu.add(item);
-        }
+    }
 
-        private void createEditMenu()
+    private void createEditMenu()
     {
         JMenuItem item;
 
@@ -132,12 +216,9 @@ public class EncryptionUI extends JFrame implements ActionListener{
         item.addActionListener(this);
         editMenu.add(item);
 
-        item = new JMenuItem("Add Message");
-        item.addActionListener(this);
-        editMenu.add(item);
     }
 
-        private void createJButtons()
+    private void createJButtons()
     {
         encryptButton = new JButton("Encrypt Message");
         add(encryptButton);
